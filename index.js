d016da2ac2a0dd89d5e7c2acdb810c30de7c4b35
Siddique Abdul <szuber558@gmail.com>
2025-06-19 13:28:48 +0530

    2nd commit

--- index.js
+++ index.js
@@ -2,6 +2,47 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const githubData = {
+  login: "CyberAbdul-Siddiqui",
+  id: 81949770,
+  node_id: "MDQ6VXNlcjgxOTQ5Nzcw",
+  avatar_url: "https://avatars.githubusercontent.com/u/81949770?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/CyberAbdul-Siddiqui",
+  html_url: "https://github.com/CyberAbdul-Siddiqui",
+  followers_url: "https://api.github.com/users/CyberAbdul-Siddiqui/followers",
+  following_url:
+    "https://api.github.com/users/CyberAbdul-Siddiqui/following{/other_user}",
+  gists_url: "https://api.github.com/users/CyberAbdul-Siddiqui/gists{/gist_id}",
+  starred_url:
+    "https://api.github.com/users/CyberAbdul-Siddiqui/starred{/owner}{/repo}",
+  subscriptions_url:
+    "https://api.github.com/users/CyberAbdul-Siddiqui/subscriptions",
+  organizations_url: "https://api.github.com/users/CyberAbdul-Siddiqui/orgs",
+  repos_url: "https://api.github.com/users/CyberAbdul-Siddiqui/repos",
+  events_url:
+    "https://api.github.com/users/CyberAbdul-Siddiqui/events{/privacy}",
+  received_events_url:
+    "https://api.github.com/users/CyberAbdul-Siddiqui/received_events",
+  type: "User",
+  user_view_type: "public",
+  site_admin: false,
+  name: null,
+  company: null,
+  blog: "",
+  location: null,
+  email: null,
+  hireable: null,
+  bio: null,
+  twitter_username: null,
+  public_repos: 16,
+  public_gists: 0,
+  followers: 0,
+  following: 0,
+  created_at: "2021-04-05T05:56:13Z",
+  updated_at: "2025-06-18T14:03:29Z",
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -15,6 +56,9 @@ app.get("/login", (req, res) => {
 });
 app.get("/youtube", (req, res) => {
   res.send("<h2>This is a YouTube Page</h2>");
+});
+app.get("/github", (req, res) => {
+  res.json(githubData);
 });
 
 app.listen(process.env.PORT, () => {
